35269caca3762db917aad3852dc328c54fa3ae8b
ylc-s <m.yamaguchi@ylc.co.jp>
2026-02-08 15:08:49 +0900

    Add initial Spring Boot project

---
+++ progress-management-system/src/test/java/com/ylc/progress_management_system/ProgressManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylc.progress_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
